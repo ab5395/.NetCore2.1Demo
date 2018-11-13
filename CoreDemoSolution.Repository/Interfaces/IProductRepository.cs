9ee41e2391b28ac835ea6a8fa78c2495f5e06089
bajay <bajay@C229>
2018-11-13 13:03:10 +0530

    Add interfaces and repositories

---
+++ CoreDemoSolution.Repository/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CoreDemoSolution.Repository.Interfaces
+{
+    using CoreDemoSolution.Data.Classes;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IProductRepository : IRepository<Product>
+    {
+        IEnumerable<Product> GetProductList();
+
+        Task<Product> GetProductById(int ProductId);
+    }
+}
---
+++ CoreDemoSolution.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿
+namespace CoreDemoSolution.Repository.Repositories
+{
+    using CoreDemoSolution.Data;
+    using CoreDemoSolution.Data.Classes;
+    using CoreDemoSolution.Repository.Interfaces;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Product> GetProductList()
+        {
+            return _db.Products;
+        }
+
+        public async Task<Product> GetProductById(int ProductId)
+        {
+            return await _db.Products.FirstOrDefaultAsync(x => x.ProductId == ProductId);
+        }
+    }
+}
